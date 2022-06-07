cfb07084e7b0d33d52785a844453b386fd086b1b
JadenSnow9 <106861742+JadenSnow9@users.noreply.github.com>
2022-06-07 19:05:47 +0200

    Revert "revert test"
    This reverts commit 31a14931c78b001b941c9fcc8720ea807a6cc830.

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const passport = require("passport");
-const session = require("express-session");
-const path = require("path");
-const mongoose = require("mongoose");
-const MongoStore = require("connect-mongo");
-const flash = require("express-flash");
-const connectDB = require("./config/db");
+const express = require('express')
+const dotenv = require('dotenv')
+const passport =require('passport')
+const session = require('express-session')
+const path = require('path')
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')
+const flash = require('express-flash')
+const connectDB = require('./config/db')
 
 //load config
-dotenv.config({ path: "./config/config.env" });
-require("./config/passport")(passport);
+dotenv.config({path:'./config/config.env'})
+require('./config/passport')(passport)
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.set("view-engine", "ejs");
+app.set('view-engine','ejs')
 //body parser
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:false}))
 //use flash
-app.use(flash());
+app.use(flash())
 
 //sessions
-app.use(
-  session({
+app.use(session({
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  })
-);
+    
+}))
 //passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-//set glopal variables
-app.use(function (req, res, next) {
-  res.locals.user = req.user || null;
-  next();
-});
+app.use(passport.initialize())
+app.use(passport.session())
+//set glopal variables 
+app.use(function(req,res,next){
+    res.locals.user= req.user || null
+    next()
+})
 
 //static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,'public')))
 
 //routes
-app.use("/", require("./routes/index"));
-app.use("/", require("./routes/auth"));
-app.use("/admin", require("./routes/admin"));
+app.use('/', require('./routes/index'))
+app.use('/', require('./routes/auth'))
+app.use('/admin', require('./routes/admin'))
+  
 
-const PORT = process.env.PORT;
 
-app.listen(PORT, console.log(`app is running on  http:/localhost/${PORT} `));
+const PORT=process.env.PORT
+
+app.listen(PORT,console.log(`app is running on  http:/localhost/${PORT} `))
--- routes/admin.js
+++ routes/admin.js
@@ -1,99 +1,43 @@
-const express = require("express");
-const argon2 = require("argon2");
-//const { redirect } = require('express/lib/response')
-const session = require("express-session");
-const fs = require("fs");
-const path = require("path");
-const multer = require("multer");
-const { isAdmin } = require("../middleware/adminAuth");
-const Product = require("../models/Product");
-const router = express.Router();
+const express = require('express')
+const argon2 = require('argon2')
+const { redirect } = require('express/lib/response')
+const session = require('express-session');
+const{isAdmin} = require('../middleware/adminAuth')
+const router = express.Router()
 //const {ensureAuth, ensureGuest} = require('../middleware/auth');
 
-//GET log in
-router.get("/login", (req, res) => {
-  res.render("admin/login.ejs");
-});
+
+//GET login
+router.get('/login',(req,res)=>{
+    res.render('admin/login.ejs')
+})
 //POST log in
 
-router.post("/login", async (req, res) => {
-  let hash = await argon2.hash(req.body.password);
-  if (
-    req.body.email == process.env.ADMIN_EMAIL &&
-    (await argon2.verify(process.env.ADMIN_PASSWORD, req.body.password))
-  ) {
-    req.session.isAdmin = true;
-    res.redirect("/admin");
-  } else {
-    console.log(process.env.ADMIN_PASSWORD);
-    console.log(hash);
-    res.redirect("/admin/login");
-  }
-});
+router.post('/login', async(req,res)=>{
 
-//GET /admin
-router.get("/", isAdmin, (req, res) => {
-  res.render("admin/index.ejs");
-});
-
-//log out
-
-router.get("/logout", isAdmin, (req, res) => {
-  req.session.isAdmin = false;
-  res.redirect("/admin/login");
-});
-
-// set up multer to upload product's images
-let storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
-
-let upload = multer({ storage: storage });
-
-//GET /admin/add
-router.get("/add", isAdmin, (req, res) => {
-  res.render("admin/addProduct.ejs");
-});
-
-//POST /admin/add
-router.post("/add", isAdmin, upload.single("image"), (req, res, next) => {
-  const newProduct = {
-    title: req.body.title,
-    description: req.body.desc,
-    quantity: parseInt(req.body.quantity),
-    price: parseInt(req.body.price),
-    img: {
-      data: fs.readFileSync(
-        path.join(__dirname, "..", "uploads", req.file.filename)
-      ),
-      contentType: "image/png",
-    },
-  };
-  Product.create(newProduct, (err, item) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // item.save();
-      res.redirect("/admin/all");
+    let hash = await argon2.hash(req.body.password)
+    if(req.body.email==process.env.ADMIN_EMAIL && await argon2.verify(process.env.ADMIN_PASSWORD, req.body.password)){
+        req.session.isAdmin=true
+        res.redirect('/admin') 
+        
     }
-  });
-});
-
-//GET /admin/products
-router.get("/products", isAdmin, (req, res) => {
-  Product.find({}, (err, products) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("An error occurred", err);
-    } else {
-      res.render("admin/products.ejs", { products: products });
+    else{
+        console.log(process.env.ADMIN_PASSWORD)
+        console.log(hash)
+        res.redirect('/admin/login')
     }
-  });
-});
+})
 
-module.exports = router;
+//GET login
+router.get('/',isAdmin,(req,res)=>{
+    res.render('admin/index.ejs')
+})
+
+//log out 
+
+router.get('/logout',isAdmin,(req,res)=>{
+    req.session.isAdmin= false
+    res.redirect('/admin/login')
+})
+
+module.exports = router 
